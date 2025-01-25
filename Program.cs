c3576bad286c9edc22ab5e718bd264f133139b96
Yeimss <jamesherrera364@gmail.com>
2025-01-25 12:21:37 -0500

    Conectamos a una base de datos en memoria y también nos conectamos a una base de datos real

--- Program.cs
+++ Program.cs
@@ -1,6 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Platzi_EF.Models.Context;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
+builder.Services.AddSqlServer<TareasContext>("Data source=YEIMS;Initial Catalog=TareasDb;user id=sa;password=1234;TrustServerCertificate=True");
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/dbconexion", async ([FromServices] TareasContext dbContext) => {
+    dbContext.Database.EnsureCreated();
+    return Results.Ok($"Base de datos en memoria: {dbContext.Database.IsInMemory()}");
+});
 
 app.Run();
