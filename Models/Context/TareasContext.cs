28abe1e17209282a8b82e024e609393e476714af
Yeimss <jamesherrera364@gmail.com>
2025-01-25 01:19:35 -0500

    Configuracion inicial del context

---
+++ Models/Context/TareasContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Platzi_EF.Models.Context;
+
+public class TareasContext : DbContext 
+{
+    public DbSet<Categoria> Categorias { get; set; }
+    public DbSet<Tarea> Tareas { get; set; }
+
+    public TareasContext(DbContextOptions<TareasContext> options) : base(options) {}
+    
+}
