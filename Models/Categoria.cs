62eb93dbdb2bea83687496e33a65bc97cb6f941d
Yeimss <jamesherrera364@gmail.com>
2025-01-25 12:00:19 -0500

    Se agregan DataAnnotations

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,8 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Platzi_EF.Models;
 
-public class Categoria{
+public class Categoria
+{
+    [Key]
     public Guid CategoriaId { get; set; }
+    [Required]
+    [MaxLength(60)]
     public string Nombre { get; set; }
+    [MaxLength(250)]
     public string Descripcion { get; set; }
     public virtual ICollection<Tarea> Tareas { get; set; }
 }
